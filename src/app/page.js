bf08b77a3778e1379fc0004773b39917647a15e4
varun-s20 <varun.jcfanx@gmail.com>
2024-07-20 18:52:57 +0530

    Init next, tailwind, aceternity components

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import { ThreeDCardDemo } from "./card/page";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <ThreeDCardDemo />
+    </>
+  );
+}
